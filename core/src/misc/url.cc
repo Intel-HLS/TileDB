ec23f3d03bc17524a87ce789dd65ec54a4e89960
Nalini Ganapati <nganapati@gmail.com>
2018-01-12 01:26:24 +0000

    Adding copyright from UCLA since some code was developed with support from UCLA

--- core/src/misc/url.cc
+++ core/src/misc/url.cc
@@ -6,6 +6,7 @@
  * The MIT License
  *
  * @copyright Copyright (c) 2017 Omics Data Automation, Inc.
+ *                      (c) 2017 UCLA All rights reserved
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
